74aff6f25b2e03de84462c028be450112aa69a6f
cpfiro <ceponce@github.com>
2013-05-07 01:03:29 -0700

    Added Direction and thoughts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,48 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+
+
+    /*
+    tic tac toe sudo code and thoughts...
+
+    here we go...
+
+    First:
+        we will need recursion... yes. But what will be the layout?
+        Probably continue the use of a string... it will be the key and
+        the solution will be the definition.
+
+
+        key as... String str = "012345678"
+
+        -the values in the string are going to be ... {-1, 0, 1}
+
+            -this way we know the value of our game.
+            -we need to develop checking methods.
+            -set up our red alert method
+            -set up opponents red alert system. (this needs to be optimized)
+
+            -Ultimately we will want to put the other player in multiple red alerts at the same time.
+            -we need to be careful on maximizing our red alerts. they can be dependent on one node and
+             that is unacceptable. It needs to be like the evil hangman where, we have lots of red alerts,
+             we play the spot that will lead to more secure multiple red alerts.
+
+        - we will probably start at "-10000000000"
+
+            -to get the hang of a game.
+            -computer starts
+            -client will do smart moves "attempt to avoid red alert scenario's"
+
+        - order of priorities
+
+            -if winning opportunity... take win
+            -if red alert... block it
+            -else optimize victory (optimize opponents red alerts)
+
+
+
+     */
+
     }
 }
